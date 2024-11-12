1c1d3b1d3b25d195c82182ea701b53a69265ec83
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-11-12 09:29:47 -0700

    updated question 4

--- modifications/wk07exercises.sql
+++ modifications/wk07exercises.sql
@@ -30,6 +30,28 @@ USE sakila;
 -- ON       c.customer_id = <2>
 -- WHERE    date(r.rental_date) = '2005-06-14'
 -- ORDER BY <3> <4>;
+-- To achieve the following results:
+-- +---------------------------------------+---------------------+
+-- | email                                 | return_date         |
+-- +---------------------------------------+---------------------+
+-- | DANIEL.CABRAL@sakilacustomer.org      | 2005-06-23 22:00:38 |
+-- | TERRANCE.ROUSH@sakilacustomer.org     | 2005-06-23 21:53:46 |
+-- | MIRIAM.MCKINNEY@sakilacustomer.org    | 2005-06-21 17:12:08 |
+-- | GWENDOLYN.MAY@sakilacustomer.org      | 2005-06-20 02:40:27 |
+-- | JEANETTE.GREENE@sakilacustomer.org    | 2005-06-19 23:26:46 |
+-- | HERMAN.DEVORE@sakilacustomer.org      | 2005-06-19 03:20:09 |
+-- | JEFFERY.PINSON@sakilacustomer.org     | 2005-06-18 21:37:33 |
+-- | MATTHEW.MAHAN@sakilacustomer.org      | 2005-06-18 05:18:58 |
+-- | MINNIE.ROMERO@sakilacustomer.org      | 2005-06-18 01:58:34 |
+-- | SONIA.GREGORY@sakilacustomer.org      | 2005-06-17 21:44:11 |
+-- | TERRENCE.GUNDERSON@sakilacustomer.org | 2005-06-17 05:28:35 |
+-- | ELMER.NOE@sakilacustomer.org          | 2005-06-17 02:11:13 |
+-- | JOYCE.EDWARDS@sakilacustomer.org      | 2005-06-16 21:00:26 |
+-- | AMBER.DIXON@sakilacustomer.org        | 2005-06-16 04:02:56 |
+-- | CHARLES.KOWALSKI@sakilacustomer.org   | 2005-06-16 02:26:34 |
+-- | CATHERINE.CAMPBELL@sakilacustomer.org | 2005-06-15 20:43:03 |
+-- +---------------------------------------+---------------------+
+-- 16 rows in set (0.05 sec)
 -- ------------------------------------------------------------------------
 
 
