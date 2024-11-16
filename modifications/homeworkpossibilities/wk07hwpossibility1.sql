82b24168b88952237057b9f7a9bd9f0afd32910b
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-11-15 20:28:18 -0700

    created 2 possibilities for week 7 homework. I'm more passionate about the second possibility.

---
+++ modifications/homeworkpossibilities/wk07hwpossibility1.sql
@@ -0,0 +1,25 @@
+-- Week 7 questions
+USE airportdb;
+-- --------------------------------------------------------------------------
+-- 1. What are the all the bookings for flight #2 (Flightno: AF1377)?
+-- --------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------
+-- 2. What are all the airlines in the United States?
+-- --------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------
+-- 3. What are all the flights that are not operated by United Airlines?
+-- --------------------------------------------------------------------------
+
+-- --------------------------------------------------------------------------
+-- 4. What are all the flights that are not operated by United Airlines and
+-- have a flight time greater than 2 hours?
+-- --------------------------------------------------------------------------
+
+-- --------------------------------------------------------------------------
+-- 5. What are all the flights that are not operated by United Airlines and
+-- have a flight time greater than 2 hours and are not to or from the
+-- United States?
---
+++ modifications/homeworkpossibilities/wk07hwpossibility2.sql
@@ -0,0 +1,41 @@
+-- Week 7 questions
+USE airportdb;
+-- --------------------------------------------------------------------------
+-- 1. What are the unique last names of our employees?
+--    List them in alphabetical order.
+-- --------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------
+-- 2. What are the airlines and which airports are they based in?
+--    List the airlines in alphabetical order.
+-- --------------------------------------------------------------------------
+
+-- --------------------------------------------------------------------------
+-- 3. What are the first 20 airports that are based in the United States?
+-- --------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------
+-- 4. What are the top 10 airports without an IATA code?
+-- --------------------------------------------------------------------------
+
+-- --------------------------------------------------------------------------
+-- 5. What are the flights that depart between 10:00 and 10:15?
+--    Sort the results by departure time.
+-- --------------------------------------------------------------------------
+
+-- --------------------------------------------------------------------------
+-- 6. What are the flights that arrive between 20:00 and 20:15 and are not
+--    flown on Monday?
+--    Sort the results by departure time.
+-- --------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------
+-- 7. Marilyn is trying to schedule a flight that departs sometime between
+--    3PM and 4PM and arrives between 6PM and 7PM on Wednesday or Thursday.
+--    She also wants to avoid flights that are operated by Cyprus Airlines. 
+--    Can you help her?
+--    Sort the results by departure time.
+-- --------------------------------------------------------------------------
