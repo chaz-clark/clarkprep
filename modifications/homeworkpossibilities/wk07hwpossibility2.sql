c22cc63d20e621ddf8377dba3cbfce916a19c3a6
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-11-17 18:19:23 -0700

    updated questions

--- modifications/homeworkpossibilities/wk07hwpossibility2.sql
+++ modifications/homeworkpossibilities/wk07hwpossibility2.sql
@@ -11,31 +11,42 @@ USE airportdb;
 --    List the airlines in alphabetical order.
 -- --------------------------------------------------------------------------
 
+
 -- --------------------------------------------------------------------------
 -- 3. What are the first 20 airports that are based in the United States?
+--    Display the airport name and country.
 -- --------------------------------------------------------------------------
 
 
 -- --------------------------------------------------------------------------
 -- 4. What are the top 10 airports without an IATA code?
+--    Display the airport name, IATA code, and ICAO code.
 -- --------------------------------------------------------------------------
 
+
 -- --------------------------------------------------------------------------
--- 5. What are the flights that depart between 10:00 and 10:15?
+-- 5. What are the flights that depart between 10:00 and 10:15 on Monday?
 --    Sort the results by departure time.
+--    List the following columns:
+--    | Flight Number | Departure Time | Arrival Time | Airline | Monday |
 -- --------------------------------------------------------------------------
+
 
 -- --------------------------------------------------------------------------
 -- 6. What are the flights that arrive between 20:00 and 20:15 and are not
 --    flown on Monday?
 --    Sort the results by departure time.
+--    List the following columns:
+--    | Flight Number | Departure Time | Arrival Time | Airline | Monday |
 -- --------------------------------------------------------------------------
 
 
--- --------------------------------------------------------------------------
+-- ------------------------------------------------------------------------------------------
 -- 7. Marilyn is trying to schedule a flight that departs sometime between
 --    3PM and 4PM and arrives between 6PM and 7PM on Wednesday or Thursday.
 --    She also wants to avoid flights that are operated by Cyprus Airlines. 
 --    Can you help her?
 --    Sort the results by departure time.
--- --------------------------------------------------------------------------
+--    List the following columns:
+--    | Flight Number | Departure Time | Arrival Time | Airline | Wednesday | Thursday |
+-- ------------------------------------------------------------------------------------------
