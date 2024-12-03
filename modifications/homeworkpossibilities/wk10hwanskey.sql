0107fd37dbe9db361f7344b6e6ef162f34f27091
NicholasBoss <NicholasBoss0914@gmail.com>
2024-12-02 19:38:44 -0700

    added in USE statement

--- modifications/homeworkpossibilities/wk10hwanskey.sql
+++ modifications/homeworkpossibilities/wk10hwanskey.sql
@@ -1,3 +1,6 @@
+-- week 10 questions
+USE airportdb;
+
 -- ---------------------------------------------------------------------------
 -- 1. Who are our frequent fliers that reside in the U.K. and have flown
 --    somewhere in the world from the U.K.?
